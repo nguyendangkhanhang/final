01a2e0f93f27308967b0cbb0ca4e90a9d368e6de
Hang <khanhhang2409@gmail.com>
2025-02-18 09:11:19 +0700

    Split admin

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import cors from 'cors'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
